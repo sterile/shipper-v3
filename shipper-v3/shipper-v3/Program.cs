f750347d8da4533714f9c4bdf66113201abe0f4e
Brandon Berney <brandonberney@protonmail.com>
2020-09-21 17:13:01 -0400

    Program: redo test data

--- shipper-v3/shipper-v3/Program.cs
+++ shipper-v3/shipper-v3/Program.cs
@@ -9,12 +9,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
 
-namespace Prog0
+namespace shipper_v3
 {
     class Program
     {
@@ -22,28 +18,30 @@ namespace Prog0
         // Postcondition: Small list of Parcels is created and displayed
         static void Main(string[] args)
         {
-            Address a1 = new Address("  John Smith  ", "   123 Any St.   ", "  Apt. 45 ", 
-                "  Louisville   ", "  KY   ", 40202); // Test Address 1
-            Address a2 = new Address("Jane Doe", "987 Main St.", 
-                "Beverly Hills", "CA", 90210); // Test Address 2
-            Address a3 = new Address("James Kirk", "654 Roddenberry Way", "Suite 321", 
-                "El Paso", "TX", 79901); // Test Address 3
-            Address a4 = new Address("John Crichton", "678 Pau Place", "Apt. 7",
-                "Portland", "ME", 04101); // Test Address 4
-
-            Letter l1 = new Letter(a1, a3, 0.50M); // Test Letter 1
-            Letter l2 = new Letter(a2, a4, 1.20M); // Test Letter 2
-            Letter l3 = new Letter(a4, a1, 1.70M); // Test Letter 3
-
-            List<Parcel> parcels = new List<Parcel> {l1, l2, l3}; // Test list of parcels
-
-            // Display data
-            WriteLine("Program 0 - List of Parcels\n");
-
-            foreach (Parcel p in parcels)
+            // List of sample addresses
+            List<Address> homes = new List<Address>
             {
-                WriteLine(p);
-                WriteLine("--------------------");
+                new Address("Helen C. Bice", "1163 Thompson Drive", "El Sobrante", "CA", 94803),
+                new Address("Teresa T. Johnson", "3542 Farland Street", "Apt 101", "Westborough", "MA", 01581),
+                new Address("Troy H. Thomas", "1299 Saints Alley", "Plant City", "FL", 33564),
+                new Address("Susan K. McCrady", "3118 Chenoweth Drive", "Apt B3", "Clarksville", "TN", 37040)
+            };
+
+            // List of sample letters using addresses
+            List<Package> packages = new List<Package>
+            {
+                new GroundPackage(homes[0], homes[1], 30, 42, 18, 60),
+                new NextDayAirPackage(homes[0], homes[3], 40, 20, 60, 74, 10M),
+                new TwoDayAirPackage(homes[2], homes[1], 20, 25, 50, 75, TwoDayAirPackage.Delivery.Early),
+                new TwoDayAirPackage(homes[2], homes[3], 40, 40, 20, 76, TwoDayAirPackage.Delivery.Saver),
+            };
+
+            Console.WriteLine("Program 1A");
+
+            foreach (Package package in packages)
+            {
+                Console.WriteLine(package.ToString());
+                Console.WriteLine(String.Empty.PadLeft(60, '-'));
             }
         }
     }
