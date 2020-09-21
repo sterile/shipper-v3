fc35194b066c80faf68e7256d030155090217b60
Brandon Berney <brandonberney@protonmail.com>
2020-09-21 14:29:07 -0400

    GroundPackage: create the GroundPackage class

--- shipper-v3/shipper-v3/GroundPackage.cs
+++ shipper-v3/shipper-v3/GroundPackage.cs
@@ -6,5 +6,38 @@ namespace shipper_v3
 {
     class GroundPackage : Package
     {
+        public GroundPackage(Address origin, Address destination, double length, double width, double height, double weight) : base(origin, destination, length, width, height, weight)
+        {
+            // This area is left blank. No additional constructors needed.
+        }
+        public int ZoneDistance
+        {
+            get
+            {
+                const int DIVIDER = 10000; // Cheat to get us a whole number
+                int larger,   // The larger number
+                    smaller,  // The smaller number
+                    distance; // The distance (based on ZIP) between destinations
+
+                larger = Math.Max(OriginAddress.Zip, DestinationAddress.Zip);
+                smaller = Math.Min(OriginAddress.Zip, DestinationAddress.Zip);
+
+                distance = (larger / DIVIDER) - (smaller / DIVIDER);
+
+                return distance;
+            }
+        }
+
+        public override decimal CalcCost()
+        {
+            const double DIMENSION_CHARGE = 0.15,
+                DISTANCE_CHARGE = 0.07;
+
+            const int ZONE_MIN = 1;
+
+            return Convert.ToDecimal((DIMENSION_CHARGE * (Length + Width + Height)) + (DISTANCE_CHARGE * (ZoneDistance + ZONE_MIN) * Weight));
+        }
+
+        public override string ToString() => base.ToString() + $"{Environment.NewLine}Zone Distance: {ZoneDistance}";
     }
 }
