cf32b84246743cabd82718d44128432e921d7f2e
Brandon Berney <brandonberney@protonmail.com>
2020-09-21 14:38:08 -0400

    AirPackage: create the AirPackage class

--- shipper-v3/shipper-v3/AirPackage.cs
+++ shipper-v3/shipper-v3/AirPackage.cs
@@ -6,5 +6,19 @@ namespace shipper_v3
 {
     abstract class AirPackage : Package
     {
+        private const int HEAVY = 75,
+            LARGE = 100;
+        public AirPackage(Address origin, Address destination, double length, double width, double height, double weight) : base(origin, destination, length, width, height, weight)
+        {
+            // This area is left blank. No additional constructors needed.
+        }
+
+        public bool IsHeavy() => Weight >= HEAVY;
+
+        public bool IsLarge() => (Length + Width + Height) >= LARGE;
+
+        public override string ToString() => base.ToString() +
+            $"{Environment.NewLine}Heavy: {IsHeavy()}" +
+            $"{Environment.NewLine}Large: {IsLarge()}";
     }
 }
